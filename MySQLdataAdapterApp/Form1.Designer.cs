09e119b243f283a760b50c0acca01a8165f703e2
wimbaert <50639680+wimbaert@users.noreply.github.com>
2020-05-07 18:59:38 +0200

    versie 1.0
    Eerste versie van opdracht voor 6IT

--- MySQLdataAdapterApp/Form1.Designer.cs
+++ MySQLdataAdapterApp/Form1.Designer.cs
@@ -29,25 +29,53 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.DgvProducten = new System.Windows.Forms.DataGridView();
+            this.BtnUpdateTabel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvProducten)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 375);
+            this.button1.Location = new System.Drawing.Point(12, 387);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(178, 51);
             this.button1.TabIndex = 0;
             this.button1.Text = "Select * from producten";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DgvProducten
+            // 
+            this.DgvProducten.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DgvProducten.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvProducten.Location = new System.Drawing.Point(12, 12);
+            this.DgvProducten.Name = "DgvProducten";
+            this.DgvProducten.RowHeadersWidth = 51;
+            this.DgvProducten.RowTemplate.Height = 24;
+            this.DgvProducten.Size = new System.Drawing.Size(1023, 369);
+            this.DgvProducten.TabIndex = 1;
+            // 
+            // BtnUpdateTabel
+            // 
+            this.BtnUpdateTabel.Location = new System.Drawing.Point(197, 388);
+            this.BtnUpdateTabel.Name = "BtnUpdateTabel";
+            this.BtnUpdateTabel.Size = new System.Drawing.Size(198, 50);
+            this.BtnUpdateTabel.TabIndex = 2;
+            this.BtnUpdateTabel.Text = "Update wijzigingen";
+            this.BtnUpdateTabel.UseVisualStyleBackColor = true;
+            this.BtnUpdateTabel.Click += new System.EventHandler(this.BtnUpdateTabel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1047, 450);
+            this.Controls.Add(this.BtnUpdateTabel);
+            this.Controls.Add(this.DgvProducten);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvProducten)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +83,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView DgvProducten;
+        private System.Windows.Forms.Button BtnUpdateTabel;
     }
 }
 
--- MySQLdataAdapterApp/Form1.cs
+++ MySQLdataAdapterApp/Form1.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace MySQLdataAdapterApp
 {
     public partial class Form1 : Form
     {
+        //objecten van belang bij het maken van een connectie met de database
+        MySqlConnection myConnection;
+        MySqlDataAdapter myDataAdapter;
+        MySqlCommandBuilder myCommandBuidler;
+        DataTable myTable;
+        string connectionString;
+        string selectQuery = "SELECT productNaam, productStock, beschikbaar FROM producten";
+
+
         public Form1()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myConnection = new MySqlConnection(connectionString);
+            myDataAdapter = new MySqlDataAdapter(selectQuery, myConnection);
+            myCommandBuidler = new MySqlCommandBuilder(myDataAdapter);
+            myTable = new DataTable();
+            myDataAdapter.Fill(myTable);
+            DgvProducten.DataSource = myTable;
+        }
+
+        private void BtnUpdateTabel_Click(object sender, EventArgs e)
+        {
+            DataTable myChanges = myTable.GetChanges();
+            myDataAdapter.Update(myChanges);
+            myTable.AcceptChanges();
         }
     }
 }
